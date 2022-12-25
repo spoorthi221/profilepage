3e2caa8b426deb0444d3099c4888ab5ed7990da8
spoorthi221 <83546939+spoorthi221@users.noreply.github.com>
2022-12-25 21:47:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
  </nav>
 </header>
 <div id="center">
-<div id="center-set"> - By Santose Naval
+<div id="center-set"> - By Spoorthi V
 <div id="signup">
 <div id="signup-st">
 <div align="center">
